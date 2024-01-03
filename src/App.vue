3ada583044fe369d06978d17e3a6bce0d4fa1ed7
novrain <novrainn@gmail.com>
2024-01-03 18:26:57 +0800

    :rocket: TabPanel component.

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,40 @@
 <template>
   <div class="container">
     <h4>Vue3 Lumino Widget</h4>
-    <h6>Drag and drop the tab item</h6>
+    <h6>BoxPanel with dock layout Drag and drop the tab item</h6>
     <h6>Current active: {{ active?.item.name || 'none' }}</h6>
     <LuminoBoxPanel>
       <LuminoWidget v-for="item in items"
                     :key="item.id"
-                    @close="onLuminoWidgetClose"
-                    @active="onLuminoWidgetActive"
-                    @show="onLuminoWidgetShow"
+                    @close="onWidgetInBoxPanelClose"
+                    @active="onWidgetInBoxPanelActive"
+                    @show="onWidgetInBoxPanelShow"
                     :title-active-class="activeClass"
                     :closable="item.closable"
                     :item="item">
         <p class="item-component">{{ item.name }}</p>
       </LuminoWidget>
     </LuminoBoxPanel>
+
+    <h6>TabPanel</h6>
+    <LuminoTabPanel>
+      <LuminoWidget v-for="item in tabItems"
+                    :key="item.id"
+                    @close="onWidgetInTabPanelClose"
+                    @active="onWidgetInTabPanelActive"
+                    @show="onWidgetInTabPanelShow"
+                    :title-active-class="activeClass"
+                    :closable="item.closable"
+                    :item="item">
+        <p class="item-component">{{ item.name }}</p>
+      </LuminoWidget>
+    </LuminoTabPanel>
   </div>
 </template>
 
 <script setup lang="ts">
 import LuminoBoxPanel from "./components/LuminoBoxPanel.vue"
+import LuminoTabPanel from "./components/LuminoTabPanel.vue"
 import LuminoWidget from "./components/LuminoWidget.vue"
 import { ref } from 'vue'
 import { ItemWidget, WidgetEvent } from "./components/ItemWidget"
@@ -29,18 +44,23 @@ const items = ref([
   { id: 'id2', name: 'item2' }
 ])
 
+const tabItems = ref([
+  { id: 'tid1', name: 'item1', closable: false },
+  { id: 'tid2', name: 'item2' }
+])
+
 const active = ref<ItemWidget>()
 
-const onLuminoWidgetClose = ({ msg, widget, item }: WidgetEvent) => {
+const onWidgetInBoxPanelClose = ({ msg, widget, item }: WidgetEvent) => {
   // do some thing, then doClose item.
   widget.doClose(msg)
 }
 
-const onLuminoWidgetActive = ({ msg, widget, item }: WidgetEvent) => {
+const onWidgetInBoxPanelActive = ({ msg, widget, item }: WidgetEvent) => {
   active.value = widget
 }
 
-const onLuminoWidgetShow = ({ msg, widget, item }: WidgetEvent) => {
+const onWidgetInBoxPanelShow = ({ msg, widget, item }: WidgetEvent) => {
   active.value = widget
 }
 
@@ -51,6 +71,22 @@ setInterval(() => {
   onOff = !onOff
   activeClass.value = onOff ? 'item-active-0' : 'item-active-1'
 }, 1000)
+
+
+const activeTab = ref<ItemWidget>()
+
+const onWidgetInTabPanelClose = ({ msg, widget, item }: WidgetEvent) => {
+  // do some thing, then doClose item.
+  widget.doClose(msg)
+}
+
+const onWidgetInTabPanelActive = ({ msg, widget, item }: WidgetEvent) => {
+  activeTab.value = widget
+}
+
+const onWidgetInTabPanelShow = ({ msg, widget, item }: WidgetEvent) => {
+  activeTab.value = widget
+}
 
 </script>
 
